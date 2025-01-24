f07f2a6f69d04eccdc70234a210c91775978d36f
tuongbx <tuong.bx@metatechnologylab.io>
2025-01-24 14:02:15 +0700

    add ui login

---
+++ src/app/(routes)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>login page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/app/(routes)/admin/layout.tsx
+++ src/app/(routes)/admin/layout.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import React from "react";
+
+import { AppSidebar } from "@/components/features/admin/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import Header from "@/components/features/admin/header";
 
 export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
